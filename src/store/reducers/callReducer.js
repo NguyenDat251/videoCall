d5502974243e5b9299d2dfa604162467ae2c2760
NguyenDat251 <nguyenquocdat2511998@gmail.com>
2022-10-24 00:12:13 +0700

    add functions

--- src/store/reducers/callReducer.js
+++ src/store/reducers/callReducer.js
@@ -1,7 +1,15 @@
 import * as callActions from '../actions/callActions';
 
 const initState = {
-  localStream: null
+  localStream: null,
+  callState: callActions.callStates.CALL_UNAVAILABLE,
+  callingDialogVisible: false,
+  callerUsername: '',
+  callRejected: {
+    rejected: false,
+    reason: ''
+  },
+  remoteStream: null
 };
 
 const reducer = (state = initState, action) => {
@@ -10,6 +18,31 @@ const reducer = (state = initState, action) => {
       return {
         ...state,
         localStream: action.localStream
+      };
+    case callActions.CALL_SET_CALL_STATE:
+      return {
+        ...state,
+        callState: action.callState
+      };
+    case callActions.CALL_SET_CALLING_DIALOG_VISIBLE:
+      return {
+        ...state,
+        callingDialogVisible: action.visible
+      };
+    case callActions.CALL_SET_CALLER_USERNAME:
+      return {
+        ...state,
+        callerUsername: action.callerUsername
+      };
+    case callActions.CALL_SET_CALL_REJECTED:
+      return {
+        ...state,
+        callRejected: action.callRejected
+      };
+    case callActions.CALL_SET_REMOTE_STREAM:
+      return {
+        ...state,
+        remoteStream: action.remoteStream
       };
     default:
       return state;
